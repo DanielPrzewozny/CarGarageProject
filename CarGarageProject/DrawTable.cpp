c7557d03deb805c6dc603e84afa39e56082b92fa
Daniel Przewoźny <34861787+DanielPrzewozny@users.noreply.github.com>
2021-06-30 09:18:44 +0200

    Update DrawTable.cpp

--- CarGarageProject/DrawTable.cpp
+++ CarGarageProject/DrawTable.cpp
@@ -4,49 +4,37 @@
 #include "DrawTable.h"
 using namespace std;
 
-namespace CarProject
-{
-	int DrawTable::tableWidth = 0;
-	void DrawTable::PrintLine()
-	{
-		char prev = cout.fill('-');
-		cout.width(DrawTable::tableWidth);
-		cout << '\n';
-	}
+namespace CarProject {
+int DrawTable::tableWidth = 0;
+void DrawTable::PrintLine() {
+  char prev = cout.fill('-');
+  cout.width(DrawTable::tableWidth);
+  cout << '\n';
+}
 
-	void DrawTable::PrintRow(bool newline, string *columns)
-	{
-		int width = (DrawTable::tableWidth - sizeof(columns)) / sizeof(columns);
-		string row = "|";
+void DrawTable::PrintRow(bool newline, string *columns) {
+  int width = (DrawTable::tableWidth - sizeof(columns)) / sizeof(columns);
+  string row = "|";
 
-		for (unsigned int a = 0; a < sizeof(columns); a = a + 1)
-		{
-			row += DrawTable::AlignCentre(columns[a], width);
-		}
+  for (unsigned int a = 0; a < sizeof(columns); a = a + 1) {
+    row += DrawTable::AlignCentre(columns[a], width);
+  }
+  if (newline) {
+    cout << row << std::endl;
+  } else {
+    cout << row;
+  }
+}
 
-		if (newline)
-		{
-			cout << row << std::endl;
-		}
-		else
-		{
-			cout << row;
-		}
-	}
+string DrawTable::AlignCentre(string text, int width) {
+  text = text.length() > width ? text.substr(0, width - 3) + "..." : text;
 
-	string DrawTable::AlignCentre(string text, int width)
-	{
-		text = text.length() > width ? text.substr(0, width - 3) + "..." : text;
-
-		if (text.empty())
-		{
-			return string(width, ' ');
-		}
-		else
-		{
-			int pad1 = width - (width - text.length()) / 2;
-			int pad2 = width;
-			return string(pad1, ' ') + text + string(pad2, ' ');
-		}
-	}
+  if (text.empty()) {
+    return string(width, ' ');
+  } else {
+    int pad1 = width - (width - text.length()) / 2;
+    int pad2 = width;
+    return string(pad1, ' ') + text + string(pad2, ' ');
+  }
+}
 }
